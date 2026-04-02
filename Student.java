4da6478fe88d3d63d091c65badeaf2bc18bffe50
Shiya-Bollamma <shiyabollamma@gmail.com>
2026-04-02 12:04:24 +0530

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,22 +1,7 @@
-package student;
-import java.util.Scanner;
+package studentManagement;
 
 public class Student {
-	
-	void Show() {
-		System.out.println("hi");
-	}
-
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the Student name:");
-		String name=sc.nextLine();
-		
-		// TODO Auto-generated method stub
-		Student s=new Student();
-		s.Show();
-		
-
-	}
-
+	int id;
+	String name;
+	int marks;
 }
